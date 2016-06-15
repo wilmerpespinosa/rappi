9d754ca1d38d85721b90a76de2c39ce13300b7d3
wilmerpespinosa <wilmer@desarrollosespeciales.com>
2016-06-15 02:44:22 -0500

    menú
    se muestra solo un link en el menú para la prueba

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -29,11 +29,7 @@
 	<div id="mainmenu">
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
-				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
+				array('label'=>'Procesar', 'url'=>array('/CubeSummation/procesar')),
 			),
 		)); ?>
 	</div><!-- mainmenu -->
